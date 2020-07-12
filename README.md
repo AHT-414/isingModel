b6bc54fb6a9a4ccf038e36cf99a143a5f11b420a
Alec T <59345949+AHT-414@users.noreply.github.com>
2020-07-12 16:32:33 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # isingModel
+A 2D Monte Carlo Ising Model written in python
+
+Basis for the model comes from Schroeder: Intro to Thermal Physics (1999)
--- main.py
+++ main.py
@@ -12,20 +12,20 @@ def start():
     print ("Initializing domain\n")
     A = initialize_Array(arraySize)
 
+    print(len(A))
     return(A,arraySize)
 
 
 def initialize_Array(N):
     # Array will have dimensions of nPoints x nPoints
     nPoints = N
-    A = np.zeros((nPoints-1,nPoints-1))
+    A = np.zeros((nPoints,nPoints))
 
     # loop through array and assign each point +1 or -1
-    for i in range(0,nPoints-1):
-        for j in range(0,nPoints-1):
+    for i in range(nPoints):
+        for j in range(nPoints):
             # Generate random +1 or -1
             A[i,j] = random.choice([1,-1])
-
 
     return(A)
 
@@ -34,55 +34,81 @@ def main(domain,N):
 
     nPoints = N
     # Initialize domain temperature
-    Temp = 10.1
+    Temp = 5.1
+
+    fig = plt.gcf()
+    fig.show()
+    fig.canvas.draw()
+    ax = fig.add_subplot(111)
+    ax.set_title('Ising Domain')
+    ax.set_aspect('equal')
+    cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+    cax.get_xaxis().set_visible(False)
+    cax.get_yaxis().set_visible(False)
+    cax.patch.set_alpha(0)
+    cax.set_frame_on(False)
 
     # Begin the main, outside loop
-    for outerLoop in range(1,101):
+    for outerLoop in range(1,51):
         Temp -= 0.1
         print("Temperature = {}".format(Temp))
 
         # Go into calculation loop
-        #updateDomain(domain,nPoints)
+        domain = updateDomain(domain,nPoints,Temp)
+
+        # Plot the domain figure
+
+        plt.imshow(domain)
+        fig.canvas.draw()
+
 
 
     return(domain)
 
-def updateDomain(domain,nPoints):
-    for iteration in range(1,nPoints**2):
-        i = random.randomrange(0,nPoints-1)
-        j = random.randomrange(0,nPoints-1)
+def updateDomain(A,nPoints,Temp):
+    for iteration in range(1,10*nPoints**2):
+        i = random.randrange(nPoints)
+        j = random.randrange(nPoints)
 
-        if i == 0 and j == 0: # Top right corner
-            nu = nPoints-1
-            nd = j+1
-            nr = i+1
-            nl = nPoints-1
-        elif i == nPoints-1 and j == 0: # Top Left corner
-            nu = nPoints-1
-            nd = j+1
-            nr = 0
-            nl = i-1
-        elif i == nPoints-1 and j == nPoints-1 #bottom left corner
 
-    return()
+
+        if j == 0: #Top
+            nu = nPoints-1
+        else:
+            nu = j-1
+
+        if j == nPoints-1: # bottom
+            nd = 0
+        else:
+            nd = j-1
+
+        if i == 0: # Right
+            nr = nPoints-1
+        else:
+            nr = i-1
+
+        if i == nPoints-1: # left
+            nl = 0
+        else:
+            nl = i+1
+
+        # Calculate delta u
+        deltaU = A[i,j] * (A[i,nu] + A[i,nd] + A[nr,j] + A[nl,j])
+
+        if deltaU <= 0:
+            # If the energy difference warrents, flip
+            A[i,j] = A[i,j] * -1
+        else:
+            savingThrow = random.random()
+            if savingThrow < mt.exp(-deltaU/Temp):
+                A[i,j] = A[i,j] * -1
+
+    return(A)
 
 
 
 
 #### This part runs the porgram
+random.seed()
 [domain,N] = start()
 domain = main(domain,N)
-
-
-# Plot the domain figure
-fig = plt.figure(figsize=(9,9))
-ax = fig.add_subplot(111)
-ax.set_title('Ising Domain')
-plt.imshow(domain)
-ax.set_aspect('equal')
-cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
-cax.get_xaxis().set_visible(False)
-cax.get_yaxis().set_visible(False)
-cax.patch.set_alpha(0)
-cax.set_frame_on(False)
-plt.show()
